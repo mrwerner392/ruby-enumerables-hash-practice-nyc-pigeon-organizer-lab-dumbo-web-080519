f1578fdde53775e65ff8a20302ed5c269f0155b0
Matt Werner <mrwerner392@gmail.com>
2019-07-18 01:03:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,20 @@
 # reorganize data in a hash, changing the keys and values
 def nyc_pigeon_organizer(data)
-  new_pigeons = {}
-  new_pigeons.keys = []
+  new_data = {}
   data.keys.each do |item|
-    
+    data[item].keys.each do |item2|
+      item2.each do |array_elem|
+        if new_data[array_elem]
+          if new_data[array_elem][item]
+            new_data[array_elem][item].push(item2.to_s)
+          else
+            new_data[array_elem][item] = [item2.to_s]
+          end
+        else
+          new_data[array_elem] = {item => [item2.to_s]}
+        end
+      end
+    end
+  end
+  new_data
 end
