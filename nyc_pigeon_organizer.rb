8ab3717a3f53e790280b7af4bbd4fb00d6bfa414
Matt Werner <mrwerner392@gmail.com>
2019-07-18 01:08:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+# reorganize data in a hash, changing the keys and values
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  data.keys.each do |item|
+    data[item].keys.each do |item2|
+      data[item][item2].each do |array_elem|
+        if new_data[array_elem]
+          if new_data[array_elem][item]
+            new_data[array_elem][item].push(item2.to_s)
+          else
+            new_data[array_elem][item] = [item2.to_s]
+          end
+        else
+          new_data[array_elem] = {item => [item2.to_s]}
+        end
+      end
+    end
+  end
+  new_data
 end
